dae8cc851123e8870d8a548823946a3f2b2c9f45
Volodymyr Denchyk <devit_dvv@ukr.net>
2020-02-23 18:32:18 +0200

    fixed directory separator issue

--- src/Bundle/KernelBundle/Provider/PluginsVersionsProvider.php
+++ src/Bundle/KernelBundle/Provider/PluginsVersionsProvider.php
@@ -36,7 +36,7 @@ class PluginsVersionsProvider
             foreach ($this->plugins as $plugin) {
                 $data = get_plugin_data(sprintf('%s%s%s', WP_PLUGIN_DIR, DIRECTORY_SEPARATOR, $plugin));
                 $this->pluginsVersions[$plugin] = $data['Version'];
-                $this->pluginsVersions[explode(DIRECTORY_SEPARATOR, $plugin)[0]] = $data['Version'];
+                $this->pluginsVersions[explode('/', $plugin)[0]] = $data['Version'];
             }
         }
 
