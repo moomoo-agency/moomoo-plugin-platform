fc20e14097e8717590ecb9e590bd1e9f2aeca701
Volodymyr Denchyk <devit_dvv@ukr.net>
2020-06-07 23:27:01 +0300

    added post_type__not_in query argument

---
+++ src/Bundle/QueryBundle/Hooks/PostTypeNotInHook.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MooMoo\Platform\Bundle\QueryBundle\Hooks;
+
+use MooMoo\Platform\Bundle\HookBundle\Model\AbstractHook;
+
+class PostTypeNotInHook extends AbstractHook
+{
+    const QUERY_ARGUMENT = 'post_type__not_in';
+
+    /**
+     * @inheritDoc
+     */
+    public function getType()
+    {
+        return AbstractHook::FILTER_TYPE;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFunction()
+    {
+        $where = func_get_arg(0);
+        $query = func_get_arg(1);
+        if (isset($query->query[self::QUERY_ARGUMENT]) && is_array($query->query[self::QUERY_ARGUMENT])) {
+            global $wpdb;
+            $statuses = $query->query[self::QUERY_ARGUMENT];
+            if (!empty($statuses)) {
+                $where = sprintf(
+                    "%s AND %s.post_type NOT IN ('%s')",
+                    $where,
+                    $wpdb->posts,
+                    implode("','", $statuses)
+                );
+            }
+        }
+        return $where;
+    }
+}
