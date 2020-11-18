ad079e30e7280e51352469f72ca42a55913ceb80
Volodymyr Denchyk <vdenchyk@Volodymyrs-MacBook-Pro.local>
2020-11-18 21:56:53 +0200

    removed unnecessary methods from RestApiEndpointInterface

--- src/Bundle/RestApiBundle/Endpoint/RestApiEndpointInterface.php
+++ src/Bundle/RestApiBundle/Endpoint/RestApiEndpointInterface.php
@@ -5,16 +5,6 @@ namespace MooMoo\Platform\Bundle\RestApiBundle\Endpoint;
 interface RestApiEndpointInterface
 {
     /**
-     * @return string
-     */
-    public function getNamespace();
-
-    /**
-     * @return string
-     */
-    public function getRoute();
-
-    /**
      * @see register_rest_route()
      */
     public function registerRoutes();
