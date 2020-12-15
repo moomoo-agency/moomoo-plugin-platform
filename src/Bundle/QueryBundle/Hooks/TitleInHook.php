7c1d19d8a74e099588b7de4b514d36f60a014485
Volodymyr Denchyk <vdenchyk@Volodymyrs-MacBook-Pro.local>
2020-12-15 21:05:15 +0200

    added 'title' query argument

---
+++ src/Bundle/QueryBundle/Hooks/TitleInHook.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MooMoo\Platform\Bundle\QueryBundle\Hooks;
+
+use MooMoo\Platform\Bundle\HookBundle\Model\AbstractHook;
+
+class TitleInHook extends AbstractHook
+{
+    const QUERY_ARGUMENT = 'title';
+    
+    /**
+     * @inheritDoc
+     */
+    public function getType()
+    {
+        return AbstractHook::FILTER_TYPE;
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function getFunction()
+    {
+        $where = func_get_arg(0);
+        $query = func_get_arg(1);
+        if (isset($query->query[self::QUERY_ARGUMENT])) {
+            global $wpdb;
+            $where = sprintf(
+                "%s AND %s.post_title = '%s'",
+                $where,
+                $wpdb->posts,
+                $query->query[self::QUERY_ARGUMENT]
+            );
+        }
+        return $where;
+    }
+}
